565004fd9ee08b6dab16e3a1bb697e7aa67d414d
Annie Lace <annie.c.lace@gmail.com>
2015-10-15 17:11:57 -0700

    jquery stuff

--- jquery/js/app.js
+++ jquery/js/app.js
@@ -6,3 +6,32 @@
     http://api.openweathermap.org/data/2.5/weather?zip=98195,us&units=imperial&appid=bd82977b86bf27fb59a04b61b657fb6f
 */
 
+//when the DOM content has been loaded
+$(function() {
+    'use strict';
+    //set all the hyperlinks to open in new tabs
+    $('a').attr('target', '_blank' );
+    //method chaining - every jquery function returns the object the function was called on
+    $('article').hide().fadeIn(1000);
+    $('#toggle-article').click(function() {
+       $('article').toggle();
+       //$('article').text("some new value")  --> Would modify the value of the element
+    });
+
+    //.then is a callback function, acts like an event listener but responds to data arriving rather than something happening
+    //(don't expect the data to return right away - you have to use a callback function)
+    var url = 'http://api.openweathermap.org/data/2.5/weather?zip=98195,us&units=imperial&appid=bd82977b86bf27fb59a04b61b657fb6f';
+    $.getJSON(url)
+        .then(function(data) {
+            console.log(data);
+            //get the temperature from the JSON object
+            var temperature = data.main.temp;
+            //select the temp span by id, set its text to the rounded temperature
+            $('#temp').text(Math.round(temperature))
+        });
+
+    //Will appear before the JSON data - the next line will execute before the data comes back from the server
+    console.log("test");
+});
+
+
--- leaflet/js/app.js
+++ leaflet/js/app.js
@@ -8,3 +8,37 @@
     attribution:
     '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
 */
+
+$(function() {
+    'use strict';
+
+    function createMap(loc, zoom) {
+        //L.map creates a new map object, takes id of the div element where the map should show up
+        //set view takes and array with two elements: lattitude and longitude,
+        // and a number that represents the initial zoom level
+        var map = L.map('map').setView(loc, zoom);
+
+        //map tiles - just know this syntax and remember attribution
+        L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+            attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+        }).addTo(map);
+
+        //add a marker to the map - takes lat/long array
+        //(circles for this marker are an svg element)
+        L.circleMarker(loc).addTo(map).bindPopup('<h2>Hello</h2>');
+        //bindPopup adds and html elements in a popup
+        //like an image from a traffic camera
+    }
+
+    //make sure this browser has the geolocation function
+    if(navigator.geolocation) {
+        //takes a function to call if successful, and a function to call if there's an error
+        //this just has the success funtion
+        navigator.geolocation.getCurrentPosition(function(pos) {
+            createMap([pos.coords.latitude, pos.coords.longitude], 15)
+        });
+    }
+    else {
+        createMap([47.6097, -122.3332], 12);
+    }
+});
