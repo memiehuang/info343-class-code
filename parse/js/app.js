636ca1d89ff52cf3fb21ba4b5b5173afbee1f71c
= <=>
2015-10-22 16:50:34 -0700

    finished up parse stuff

--- parse/js/app.js
+++ parse/js/app.js
@@ -1,8 +1,11 @@
 /*
     script for the index.html file
+    look in parse docs javascript guide under destroying objects
+    (they prefer promise syntax with .then)
 */
 
 //identifies the application - can go outside the document ready function
+//these are the application key and the javascript key (can be accessed under settings)
 Parse.initialize("mYTXJJajodOwHTTpnU6OXtSJth2IWYWxEcepOO1U", "n4G4QQmtaDJZRdqzAYZUKuWjLlv4a9gdanQb2nUC");
 
 $(function() {
@@ -19,9 +22,14 @@ $(function() {
     //sorts by the time they were created
     //(orders the query)
     tasksQuery.ascending('createdAt');
+    //is like a SQL where clause
+    tasksQuery.notEqualTo('done', true);
 
     //reference to the task list element
     var tasksList = $('#tasks-list');
+
+    //reference to our rating element
+    var ratingElem = $('#rating');
 
     //reference to the error message alert
     var errorMessage = $('#error-message');
@@ -68,9 +76,28 @@ $(function() {
         //clear out list so adding new tasks doesn't duplicate previously added tasks
         tasksList.empty();
         tasks.forEach(function(task) {
-           $(document.createElement('li'))
+          var li =  $(document.createElement('li'))
                .text(task.get('title'))
-               .appendTo(tasksList);
+               //terinary operator adds "completed-task" class to task if the task is done
+               .addClass(task.get('done') ? 'completed-task' : '')
+               .appendTo(tasksList)
+               //toggles done property on click
+               //you have access to all variables and parameters declared in the containing function
+               .click(function() {
+                   //just set and save data to update it
+                   task.set('done', !task.get('done'));
+                   task.save().then(renderTasks, displayError);
+                });
+            //adds read-only rating star display next to tasks
+            $(document.createElement('span'))
+              //raty takes options as a javascript object
+              .raty({readOnly: true,
+                    //if the result of the first expression is "falsy" then use the right value
+                    //undefined will coerce to false; this allows handling of undefined case
+                    //also good for setting default argument values
+                    score: (task.get('#rating') || 1),
+                    hints: ['crap', 'awful', 'ok', 'nice', 'awesome']})
+              .appendTo(li);
         });
     }
 
@@ -86,9 +113,12 @@ $(function() {
         var task = new Task();
         //set the title property of the new task equal to the title that was typed into the input box
         task.set('title', title);
+        task.set('rating', $('#rating').raty('score'));
                                                 //clear task box after saving it (chains .thens)
         task.save().then(fetchTasks, displayError).then(function() {
             titleInput.val('');
+            //reset number of stars
+            ratingElem.raty('set', {});
         });
 
         return false;
@@ -96,6 +126,9 @@ $(function() {
 
     //go and fetch tasks from the server
     fetchTasks();
+
+    //enable rating user interface
+    ratingElem.raty();
 
     //refers to browser interval
     //set interval calls this function every number of milliseconds that its passed in
