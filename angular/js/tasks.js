e3b5401fd3f74bd6af8343a3f77348dd3d4828b2
Memie Huang <memie.huang@gmail.com>
2015-10-27 17:17:06 -0700

    classwork complete

--- angular/js/tasks.js
+++ angular/js/tasks.js
@@ -2,3 +2,35 @@
     script for the tasks.html file 
 */
 
+angular.module('Tasks', [])
+    .constant('tasksKey', 'tasks')
+    .controller('TasksController', function($scope, tasksKey){
+        'use strict';
+
+        //initialize tasks property on the scope to an empty array
+        $scope.tasks = angular.fromJson(localStorage.getItem(tasksKey)) || []; //holds all tasks
+        //initialize newTask to an empty object
+        $scope.newTask = {}; //new task object
+
+        //creates a local storage to save tasks
+        function saveTasks(){
+            localStorage.setItem(tasksKey, angular.toJson($scope.tasks))//will encode into JSON string format to save or post on the internet
+        }
+
+        //add a function to add newTask to the array
+        $scope.addTask = function(){
+            //push the current value of newTask into the tasks array
+            $scope.tasks.push($scope.newTask);
+
+            saveTasks();
+
+            //reset newTask to an empty object. clears title property and it automatically clears out title input because its a new object
+            $scope.newTask = {};
+        };
+
+        //function to toggle task done state
+        $scope.toggleDone = function(task) {
+            task.done = !task.done;
+            saveTasks();
+        }
+    });
