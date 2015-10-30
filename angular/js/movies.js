ee040090808c5104a9d4030ee3c5d7e7e3e76530
= <=>
2015-10-29 17:19:47 -0700

    angular with ui routing

--- angular/js/movies.js
+++ angular/js/movies.js
@@ -1,6 +1,29 @@
-angular.module('Movies', [])
-    //$http is for ajax requests
-    .controller('MoviesController', function($scope, $http) {
+//add the name of the ui router angular module to the list of dependencies
+angular.module('Movies', ['ui.router'])
+    .factory('moviesJSON', function($http){
+        return $http.get('data/movies-2014.min.json')
+    })
+    .config(function($stateProvider, $urlRouterProvider) {
+        $stateProvider
+            .state('list', {
+               url: '/movies',
+               templateUrl: 'views/movies-list.html',
+               controller: 'MoviesController'
+            })
+            .state('detail', {
+                url: '/movies/:index',
+                templateUrl: 'views/movies-detail.html',
+                controller: 'MovieDetailController'
+            });
+        $urlRouterProvider.otherwise('/movies');
+    })
+    .controller('MovieDetailController', function($scope, $stateParams, moviesJSON) {
+        moviesJSON.then(function(results) {
+           $scope.movie = results.data[$stateParams.index];
+        });
+    })
+    .controller('MoviesController', function($scope, moviesJSON) {
+        //parameter for the controller must match the name of the factor exactly
         //Set rating values so they can be sorted in this order
         //rather than alphabetically
         var ratingsMap = {
@@ -13,8 +36,7 @@ angular.module('Movies', [])
             'X': 6
         }
 
-        $http.get('data/movies-2014.min.json')
-            .then(function(results){
+        moviesJSON.then(function(results){
                 //map constructs and returns a new array the same size as the original array
                 //with elements that consist of whatever the passed in function returns
                 $scope.movies = results.data.map(function(movie){
