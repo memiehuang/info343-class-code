279c16882cc8b7fdff3f25bb28008df9229a362a
Memie Huang <memie.huang@gmail.com>
2015-11-12 17:07:51 -0800

    learning protractor

--- protractor/test/protractor-conf.js
+++ protractor/test/protractor-conf.js
@@ -1,5 +1,5 @@
 exports.config = {
     seleniumAddress: 'http://localhost:4444/wd/hub',
-    specs: ['*-spec.js'],
-    rootElement: 'body'
+    specs: ['*-spec.js'], //any file that ends in spec.js will be called
+    rootElement: 'body' //where your ng-app is. defaults to body, so need to specify
 };
